c56a74a73af3f6285e7c3c68fbbf9410fe070e9a
Pritish Nagpal <pritishnagpal2015@gmail.com>
2021-06-26 22:10:43 +0530

    Updated db

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const connection = mysql.createPool({
-  host: "bbjzu8715dniemmehvw6-mysql.services.clever-cloud.com",
-  user: "u1cuj0ibnxb6jp2m",
-  password: "fmnyHuPBeTh5RgYq5Et9",
-  database: "bbjzu8715dniemmehvw6",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   multipleStatements: true,
 });
 
